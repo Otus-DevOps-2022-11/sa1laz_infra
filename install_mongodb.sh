d5112f01c48c42053c74bff6732ae4df20b26bf1
Anton Konev <slz@Antons-MacBook-Pro.local>
2022-12-25 21:15:05 +0400

    DZ4. Deploying testapp in YC. try5

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -5,7 +5,8 @@ wget -qO - "https://www.mongodb.org/static/pgp/server-4.2.asc" | sudo apt-key ad
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+sleep 5
+sudo systemctl enable mongod
 sudo systemctl start mongod
 sudo systemctl enable mongod
 sudo systemctl status mongod
-sleep 20
